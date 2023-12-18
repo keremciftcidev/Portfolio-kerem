7a540048f373f0ff21821c65d8151815c30e3bb2
Kerem Ciftci <krmciftci17@gmail.com>
2023-12-18 18:06:24 +0300

    Create project

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.scss"
+
+const App = () => {
+  return <div>
+    <section><a href="">Hello</a></section>
+    <section>Parallax</section>
+    <section>Sevices</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
