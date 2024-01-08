5841861bf7731a1e62ed0aab2d87377dc67f56b6
Kerem Ciftci <krmciftci17@gmail.com>
2024-01-08 17:22:16 +0300

    Added mobile responsive design

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,6 +1,26 @@
+import { useRef, useState } from "react"
 import "./contact.scss"
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+
+    const formRef = useRef()
+    const [error, setError] = useState(false)
+    const [success, setSuccess] = useState(false)
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_78n931k', 'template_vesopdv', formRef.current, 'b2NGU5JPcvFSZzwj2')
+          .then((result) => {
+              
+              setSuccess(true)
+          }, (error) => {
+              setError(true)
+              
+          });
+      };
   return (
     <div className="contact">
         <div className="textContainer">
@@ -19,11 +39,13 @@ const Contact = () => {
             </div>
         </div>
         <div className="formContainer">
-            <form>
-                <input type="text" required placeholder="Name" />
-                <input type="email" required placeholder="Email" />
-                <textarea placeholder="Message"   rows={8}/>
+            <form ref={formRef} onSubmit={sendEmail}>
+                <input type="text" required placeholder="Name" name="name" />
+                <input type="email" required placeholder="Email"  name="email"/>
+                <textarea placeholder="Message" name="message"  rows={8}/>
                 <button>Submit</button>
+                {error && "Error" }
+                {success && "Success" }
             </form>
         </div>
     </div>
